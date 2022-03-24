44de9ef6064d8b8c2fa84666d1899abd31cff502
C. Metah <97469218+Mastodon-E@users.noreply.github.com>
2022-03-23 22:03:55 -0400

    Update myenc.py

--- Lab 7/myenc.py
+++ Lab 7/myenc.py
@@ -3,9 +3,6 @@
 from Crypto.Cipher import AES
 from Crypto.Util import Padding
 
-#key_string = 'computer########'
-#key = bytes.fromhex(key_string.encode('utf-8').hex())
-
 iv_hex_string  = 'aabbccddeeff00998877665544332211'
 iv  = bytes.fromhex(iv_hex_string)
 
@@ -19,8 +16,11 @@ file1 = open("words.txt", "r")
 Lines = file1.readlines()
 
 for line in Lines:
+   # Remove endline character
    key_string = line.strip()
-   #print(key_string)
+   
+   # If the len is less than 16, add #
+   # If the len is more than 16, ignore that key
    if (len(key_string) < 16):
        add = 16 - len(key_string)
        for x in range(add):
@@ -28,13 +28,16 @@ for line in Lines:
    elif (len(key_string) > 16):
         key_string = '################'
    
+   # Convert string to hex
    key = bytes.fromhex(key_string.encode('utf-8').hex())
    
    # Encrypt the entire data
    cipher = AES.new(key, AES.MODE_CBC, iv)                   
    ciphertext = cipher.encrypt(Padding.pad(data, 16))
    b = format(ciphertext.hex())
-
+   
+   # If the encrypted data is equal to expected cipher,
+   # that means "HOORAY!"
    if expected_ciphertext == b:
        print("Ciphertext: {0}".format(ciphertext.hex()))
        print(key_string)
